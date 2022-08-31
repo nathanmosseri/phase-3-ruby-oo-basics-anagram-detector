aecd0df194d983bd15d76d321d3be8d36b3cb308
Nathan <namo678@me.com>
2022-08-31 11:34:50 -0400

    commit tree 242272786ca6ca6af84fb125e9717558aef2399b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram 
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select {|x| x.split("").sort == @word.split("").sort}
+        # array.map do |el|
+        #     el == word
+        #     el
+        # end
+        # []
+    end
+
+end
+
+# w1 = Anagram.new('hello')
+# p w1.match
